dcf965217243628e4517c0167adf22de32df7bf3
javieratorresm <jakane.x@hotmail.com>
2020-07-30 00:30:31 -0400

    update struct

--- pages/project.vue
+++ pages/project.vue
@@ -18,16 +18,26 @@
                       <div class="line-bottom work-dis">
                           <b-row  align-h="center pl-5" >
                             <b-col sm="12" class="">
-
-                          <div class="py-5">
+                            <div class="py-5">
+                              <div class="row">
+                                  <div class="col-md-3">
+                                    <h3><span class="pt-3 white font2bold">Ciudad Sur</span></h3>
+                                    <ul class="left pt-3">
+                                      <li>Front-end</li>
+                                      <li>Vuejs</li>
+                                      <li>Nuxtjs</li>
+                                      <li>Wordpress</li>
+                                      <li>Developer</li>
+                                      <li>Web Design</li>
+                                      <li>PMO</li>
+                                    </ul>
+                                  </div>
+                                    <div class="col-md-9">
                                   <img alt="" title="" class="img-port-1 w-50  center" src="@/assets/img/ciudadsur1.png"/>
-                                  <a  class="">
+                                    </div>
                                       <div class="pt-3 work-task">
-
                                           <div class="row">
                                               <div class="col-md-3">
-                                                <h3><span class="pt-3 white font2bold">Ciudad Sur</span></h3>
-                                          <p><span class="white font1">Front-end, Vuejs, Nuxtjs, Design, UX, UI, Developer, Web Design, PMO</span></p>
                                           <img class="w-70" src="@/assets/img/fondef.svg" >
                                         </div>
 
@@ -38,34 +48,46 @@
                                       </div>
 
                                       </div>
-                                  </a>
 
                           </div>
+                        </div>
                         </b-col>
                           <!-- end : works-item -->
 
                           <b-col sm="12" class="">
-
                           <div class="py-5">
-                                  <img alt="" title="" class="img-port-1 w-50  center" src="@/assets/img/citiaps.png"/>
-                                  <a  class="">
+                            <div class="row">
+                                <div class="col-md-3">
+                                  <h3><span class="py-3 white font2bold">Portal Center of Innovation and Researh</span></h3>
+
+                              <ul class="left pt-3">
+                                <li>Front-end</li>
+                                <li>Vuejs</li>
+                                <li>Nuxtjs</li>
+                                <li>Wordpress</li>
+
+                              </ul>
+                          </div>
+                          <div class="col-md-9">
+                                  <img alt="" title="" class="w-50 img-port-1" src="@/assets/img/citiaps.png"/>
+                                </div>
+
+                                </div>
                                       <div class="pt-3 work-task">
 
                                           <div class="row">
                                               <div class="col-md-3">
-                                                <h3><span class="pt-3 white font2bold">Portal Citiaps</span></h3>
-                                          <p><span class="white font1">Front-end, Vuejs, Nuxtjs, Developer, Wordpress</span></p>
+
                                           <img class="w-70" src="" >
                                         </div>
 
-                                          <div class="col-md-8">
+                                          <div class="col-md-7">
                                             <h4><span class="pt-3 white font2bold">Description</span></h4>
                                           <p>{{description_2}}</p>
                                         </div>
                                       </div>
 
                                       </div>
-                                  </a>
 
                           </div>
                                                   </b-col>
@@ -104,8 +126,8 @@ export default {
     return {
       show: true,
       tags: ["Front-end", "Vuejs", "Nuxtjs", "Project", "Design", "UX", "UI", "Developer", "Web Design", ""],
-      description_1: "I participated in the development of the Healthy living south city Fondef project, I created the concept to be developed on the website, developing it using vuejs, nutxjs, and UX/UI user experience concepts. In addition, I  established the guidelines of the project's social media plan, advising on the creation, content, and other matters related to the design. I am currently the PMO of the technology area, in charge of the team and the 3 software modules in which the project management system operates.",
-      description_2:"Development of the main website for a company, using vuejs, vuebootstrap and nuxtjs, in addition to applying design concepts and UX / UI. Implementation of custom CRM with Wordpress, the website consults the api obtained from wp and the user can edit its content without having to enter the code.",
+      description_1: "I participated in the development of the Healthy living south city Fondef project from Chile.   I created the concept to be developed on the website, developing it using vuejs, nutxjs, and UX/UI user experience concepts. In addition, I  established the guidelines of the project's social media plan, advising on the creation, content, and other matters related to the design. I am currently the PMO of the technology area, in charge of the team and the 3 software modules in which the project management system operates.",
+      description_2:"Successfully developed the main website for a company, using vuejs, vuebootstrap, nuxtjs, and applied design concepts and UX/UI. Implemented CRM with WordPress, allowing the website to communicate with the API from wp, and letting the user edit the information in a user-friendly environment.",
       description_3: "Of course this online cv was also built by me, using vuejs and nutxjs, in addition to notions of bootstrap and usability.",
 
 
